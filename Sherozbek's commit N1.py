5f745b310f94ecb55c9dd4fb3f1a0c7d56030ffa
Lapusya <120302596+Lapusya@users.noreply.github.com>
2026-05-04 11:38:22 +0500

    Delete Sherozbek's commit N1.py

--- Sherozbek's commit N1.py
+++
@@ -1,9 +0,0 @@
-rows = int(input("Enter the number of row: "))
-columns = int(input("Enter the number of columns: "))
-symbol = input("Enter a symbol: ")
-for i in range(rows):
-    for x in range(columns):
-        print(symbol, end=" ")
-    print()
-
-print("Something new")
